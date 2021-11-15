f1052b2fb53a1eb3370eac596936adfe51d75529
Massimiliano <utzerimassimiliano@gmail.com>
2021-11-15 10:53:22 +0100

    MicroServiciosDiscoveryClient

---
+++ src/main/java/com/example/demo/PrimerProyectoMicroDiscoveryClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoMicroDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoMicroDiscoveryClientApplication.class, args);
+	}
+
+}
